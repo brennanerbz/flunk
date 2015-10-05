f7c11434f3c58033e0a4da3b2bb466f2c582aa63
Brennan Erbeznik <brennan.erbeznik@gmail.com>
2015-10-05 11:40:01 -0700

    Third column structure

--- app/routes/Home/Home.js
+++ app/routes/Home/Home.js
@@ -34,7 +34,10 @@ export default class Home extends Component {
 	render() {
 		const set_icon = require('../../assets/set_icon.png'),
 			  share_icon = require('../../assets/share_icon.png'),
-			  more_icon = require('../../assets/more_icon.png');
+			  more_icon = require('../../assets/more_icon.png'),
+			  practice_icon = require('../../assets/practice_icon_blue.png'),
+			  activity_icon = require('../../assets/activity_icon_blue.png'),
+			  online_icon = require('../../assets/profile_icon_green.png');
 		return(
 			<div className="main_content home_container">
 				<div className="page_header_wrapper">
@@ -54,23 +57,18 @@ export default class Home extends Component {
 				      	<div className="row set_row"
 				      	  	 onMouseOver={this.setActiveRow}
 				      	  	 onMouseLeave={this.removeActiveRow}>	
-
 					        <div className="col-md-1 set_col row_icon">
 					        	<img className="home_set_icon active" src={set_icon}/>
 					        </div>
-
-					        <div className="col-md-4 set_col set_name">
+					        <div className="col-md-5 set_col set_name">
 					        	<span className="overflow_ellipsis"><a>Intro to biology</a></span>
 					        </div>
-
 					        <div className="col-md-3 set_col set_author">
 					        	<span className="overflow_ellipsis">by Brennan Erbeznik</span>
 					        </div>
-
 					        <div className="col-md-2 set_col date_last_studied">
 					        	<span className="overflow_ellipsis">1 hour ago</span>
 					        </div>
-
 					        <div className={classnames('actions', {'active': this.state.active_set_row === 1})}>
 					        	<button data-toggle="tooltip" title="Share" className="btn_icon btn_outline btn_square">
 					        		<img className="icon" src={share_icon}/>
@@ -79,10 +77,69 @@ export default class Home extends Component {
 					        		<img className="icon_awksize" src={more_icon}/>
 					        	</button>
 					        </div>
-					    </div>      			 
-				  </div>
-				  <div className="home_feed supplemental col-md-3 remove_small">.col-xs-6 .col-md-4</div>
+					    </div>
+					    <div className="row set_row"
+				      	  	 onMouseOver={this.setActiveRow}
+				      	  	 onMouseLeave={this.removeActiveRow}>	
+					        <div className="col-md-1 set_col row_icon">
+					        	<img className="home_set_icon active" src={set_icon}/>
+					        </div>
+					        <div className="col-md-5 set_col set_name">
+					        	<span className="overflow_ellipsis"><a>Computer Science 101</a></span>
+					        </div>
+					        <div className="col-md-3 set_col set_author">
+					        	<span className="overflow_ellipsis">by Nathan Lomeli</span>
+					        </div>
+					        <div className="col-md-2 set_col date_last_studied">
+					        	<span className="overflow_ellipsis">7 hours ago</span>
+					        </div>
+					        <div className={classnames('actions', {'active': this.state.active_set_row === 1})}>
+					        	<button data-toggle="tooltip" title="Share" className="btn_icon btn_outline btn_square">
+					        		<img className="icon" src={share_icon}/>
+					        	</button>
+					        	<button data-toggle="tooltip" title="More" className="btn_icon btn_outline btn_square">
+					        		<img className="icon_awksize" src={more_icon}/>
+					        	</button>
+					        </div>
+					    </div>         			 				  				  
+				  </div>		  
 				</div>	
+					<div className="home_feed supplemental col-md-3 remove_small">
+					<div className="section_header">
+						<span className="section_icon"><img src={activity_icon} className="header_icon"/></span>
+						<span className="section_header_label">Activity</span>
+					</div>
+					<ul className="activity_list">
+						<li>
+							<span className="activity_item">
+								<div className="activity_action">
+									<a><img src={practice_icon} className="activity_icon"/></a>
+								</div>
+								<div className="activity_message">
+									<div className="message_content">Brennan Erbeznik practiced <a className="link">Intro to Psychology</a></div>
+									<span className="message_datetime">4 hours ago</span>
+								</div>
+							</span>
+						</li>
+					</ul>
+					<div className="section_header">
+						<span className="section_icon"><img src={online_icon} className="header_icon"/></span>
+						<span className="section_header_label">Online</span>
+					</div>
+					<ul className="activity_list">
+						<li>
+							<span className="activity_item">
+								<div className="activity_action">
+									<a><img src={practice_icon} className="activity_icon"/></a>
+								</div>
+								<div className="activity_message">
+									<div className="message_content">Brennan Erbeznik practiced <a className="link">Intro to Psychology</a></div>
+									<span className="message_datetime">4 hours ago</span>
+								</div>
+							</span>
+						</li>
+					</ul>
+					</div>			
 				</div>			
 			</div>
 		);

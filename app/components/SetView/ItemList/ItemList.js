1e2ea458558cb1587a3f6c3002dbf6aebbe53870
Brennan Erbeznik <brennan.erbeznik@gmail.com>
2015-10-16 19:09:01 -0700

    ItemList

--- app/components/SetView/ItemList/ItemList.js
+++ app/components/SetView/ItemList/ItemList.js
@@ -1,13 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 require('./ItemList.scss');
+import Item from './Item';
 
 export default class ItemList extends Component {
 	static propTypes = {
 	}
 
 	render() {
+		const { set } = this.props;
 		return(
-			<div>
+			<div className="item_list_container">
+				<ul className="item_list">
+					{set.items.map((item, i) => {
+						return <Item key={i} item={item}/>
+					})}
+				</ul>
 			</div>
 		);
 	}

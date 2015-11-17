37ec2c5c36498988a886076c0f1e210a9f69a28a
Brennan Erbeznik <brennan.erbeznik@gmail.com>
2015-11-17 10:28:48 -0800

    404 page complete

--- app/routes/ErrorPage/ErrorPage.js
+++ app/routes/ErrorPage/ErrorPage.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router'
 require('./ErrorPage.scss')
 
 export default class ErrorPage extends Component {
@@ -6,17 +7,32 @@ export default class ErrorPage extends Component {
 	}
 
 	render() {
+		const sad_face = require('../../assets/sad_face.png'),
+		  	  brand_logo = require('../../components/Header/assets/FlunkLogo.png');
 		return(
 			<div className="">
 				<div className="error_page">
-					<h1 className="error">404</h1>
-					<h1 className="not_found">Not found.</h1>
-					<p className="head error_message">
-						Looks like something went wrong!
-					</p>
-					<p className="error_message">
-						We can't find the page you're looking for. Head back <a className="link">home.</a>
-					</p>
+					<div className="error_card">
+						<div className="error_msg">
+							<img src={brand_logo} className="brand_logo"/>
+							<p className="error"><b>404. </b>That's an error</p>
+							<p className="error">To learn more about website errors, &nbsp;
+							<Link className="link"
+							   	  to="/search/concepts/error"> 
+							   	   click here.
+							</Link>
+							<br/>
+							Or, just head back &nbsp;  
+							<Link className="link"
+								  to="/"> 
+								  home.
+							</Link>
+							</p>
+						</div>
+						<div className="error_logo">
+							<img src={sad_face} className="logo"/>
+						</div>
+					</div>
 				</div>
 			</div>
 		);

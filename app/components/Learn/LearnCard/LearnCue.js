27ec552fbc5344b6ea7486786bf88999fd3cf9dd
Brennan Erbeznik <brennan.erbeznik@gmail.com>
2015-11-17 16:02:26 -0800

    blur for cue. bold for correct.

--- app/components/Learn/LearnCard/LearnCue.js
+++ app/components/Learn/LearnCard/LearnCue.js
@@ -10,15 +10,23 @@ export default class LearnCue extends Component {
 		return this.props.cue == nextProps.cue
 	}
 
-	renderCue(cue, target) {
+	renderCue(cue, target, correct) {
 		if(cue == null) return { __html: null }
-		// if(cue.toLowerCase().indexOf(target.toLowerCase()) == 0) {
-		// 	cue = cue.replace(new RegExp('(^|\\s)(' + target + ')(\\s|$)','ig'), 'What is ')
-		// 	cue = cue.replace(cue.slice(-1)[0], '?').replace("is is", 'is')
-		// }
-		if(cue.toLowerCase().indexOf(target.toLowerCase()) !== -1) {
-			cue = cue.replace(new RegExp(target + ',', "gi"), target);
-			cue = cue.replace(new RegExp('(^|\\s)(' + target + ')(\\s|$)','ig'), '$1<b class="blurry_text">&nbsp;$2&nbsp;</b>$3')
+		let index = cue.toLowerCase().indexOf(target.toLowerCase())
+		if(index !== -1) {
+			if(correct) {
+				console.log(index)
+				var re = new RegExp('(^|\\s)(' + target + ')(\\s|$)','ig')
+				if(index > 1) {
+					target = target.toLowerCase()
+				} else {
+					target = target.charAt(0).toUpperCase() + target.slice(1)
+				}
+				cue = cue.replace(re, `$1<b>${target}</b>$3`)
+			} else {
+				cue = cue.replace(new RegExp(target + ',', "gi"), target);
+				cue = cue.replace(new RegExp('(^|\\s)(' + target + ')(\\s|$)','ig'), '$1<b class="blurry_text">&nbsp;$2&nbsp;</b>$3')
+			}
 		}
 		return {
 			__html: cue
@@ -26,20 +34,16 @@ export default class LearnCue extends Component {
 	}
 
 	render() {
-		const { cue, current_slot } = this.props;
+		const { cue, current_slot, showCorrect } = this.props;
 		let target = current_slot.association.item.target;
 		return(
-			// <ReactCSSTransitionGroup transitionName="" 
-			// 					     transitionEnterTimeout={500} 
-			// 					     transitionLeaveTimeout={500}>
 			<div>
-			{
-				cue !== (undefined || null)
-				? <p key={current_slot.id} className="cue" dangerouslySetInnerHTML={::this.renderCue(cue, target)}></p>
-				: null
-			}
+				{
+					cue !== (undefined || null)
+					? <p key={current_slot.id} className="cue" dangerouslySetInnerHTML={::this.renderCue(cue, target, showCorrect)}></p>
+					: null
+				}
 			</div>
-		 	// </ReactCSSTransitionGroup>
 		);
 	}
 }

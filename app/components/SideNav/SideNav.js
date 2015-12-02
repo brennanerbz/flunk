00d10c307a4049d025dd36d9ecd5ad60683e2c09
Brennan Erbeznik <brennan.erbeznik@gmail.com>
2015-12-02 10:11:26 -0800

    testing webpack
    the issue seemed to be from working from DropBox folder

--- app/components/SideNav/SideNav.js
+++ app/components/SideNav/SideNav.js
@@ -29,7 +29,7 @@ export default class SideNav extends Component {
 			root_path;
 		pathname !== '/' ? root_path = regex.exec(pathname) : root_path = '/'
 		console.log(root_path)
-		console.log('working good')
+		console.log('working fine')
 		return(
 			<div className="sidenav">
 					<Main {...this.props}/>	
